3e2b97c4881f75aaebbf0c8e95ab7d6e582649f4
mirkobrombin <send@mirko.pm>
2022-09-11 17:14:03 +0200

    0.1.2
    fix wrong var name

--- vanilla_first_setup/main.py
+++ vanilla_first_setup/main.py
@@ -124,7 +124,7 @@ class FirstSetupApplication(Adw.Application):
 
 def main(version):
     """The application's entry point."""
-    if os.env.get("USERNAME") in ["ubuntu", "vanillaos", "vanilla-os"]:
+    if os.environ.get("USERNAME") in ["ubuntu", "vanillaos", "vanilla-os"]:
         logging.warning("Running in Live mode, closing...")
         sys.exit(0)
 
