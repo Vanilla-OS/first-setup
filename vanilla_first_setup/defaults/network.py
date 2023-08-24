f51a1bc99aea7ced4f929e76dbea9eb7f3c70a52
Mateus Melchiades <matbme@duck.com>
2023-08-23 21:29:17 -0300

    List wireless networks

--- vanilla_first_setup/defaults/network.py
+++ vanilla_first_setup/defaults/network.py
@@ -19,9 +19,48 @@
 import time
 from gettext import gettext as _
 
-from gi.repository import NM, Adw, Gtk
+from gi.repository import NM, NMA4, Adw, Gtk
 
 from vanilla_first_setup.utils.run_async import RunAsync
+
+
+@Gtk.Template(resource_path="/org/vanillaos/FirstSetup/gtk/wireless-row.ui")
+class WirelessRow(Adw.ActionRow):
+    __gtype_name__ = "WirelessRow"
+
+    signal_icon = Gtk.Template.Child()
+    secure_icon = Gtk.Template.Child()
+    connected_label = Gtk.Template.Child()
+
+    def __init__(self, ap, **kwargs):
+        super().__init__(**kwargs)
+        self.ap = ap
+
+        self.__build_ui()
+
+    def __build_ui(self):
+        # TODO: Add lock icon if connection has security
+        ssid = self.ap.get_ssid()
+        if ssid is not None:
+            ssid = ssid.get_data().decode("utf-8")
+        else:
+            ssid = ""
+
+        # We use the same strength logic as gnome-controll-center
+        strength = self.ap.get_strength()
+        if strength < 20:
+            icon_name = "network-wireless-signal-none-symbolic"
+        elif strength < 40:
+            icon_name = "network-wireless-signal-weak-symbolic"
+        elif strength < 50:
+            icon_name = "network-wireless-signal-ok-symbolic"
+        elif strength < 80:
+            icon_name = "network-wireless-signal-good-symbolic"
+        else:
+            icon_name = "network-wireless-signal-excellent-symbolic"
+
+        self.set_title(ssid)
+        self.signal_icon.set_from_icon_name(icon_name)
 
 
 @Gtk.Template(resource_path="/org/vanillaos/FirstSetup/gtk/default-network.ui")
@@ -44,6 +83,8 @@ class VanillaDefaultNetwork(Adw.Bin):
 
         self.__wired_children = []
         self.__wireless_children = []
+
+        self.__last_wifi_scan = 0
 
         # Since we have a dedicated page for checking connectivity,
         # we only need to make sure the user has some type of
@@ -83,6 +124,7 @@ class VanillaDefaultNetwork(Adw.Bin):
                     self.__add_ethernet_connection(device)
                     eth_devices += 1
                 elif device_type == NM.DeviceType.WIFI:
+                    self.__add_wifi_networks(device)
                     wifi_devices += 1
                 else:
                     continue
@@ -91,6 +133,7 @@ class VanillaDefaultNetwork(Adw.Bin):
         self.wireless_group.set_visible(wifi_devices > 0)
 
     def __refresh(self):
+        # TODO: Re-use rows to prevent list from jumping around
         for child in self.__wired_children:
             self.wired_group.remove(child)
         for child in self.__wireless_children:
@@ -116,10 +159,11 @@ class VanillaDefaultNetwork(Adw.Bin):
             case NM.DeviceState.ACTIVATED:
                 status = _("Connected")
                 connected = True
+            case NM.DeviceState.NEED_AUTH:
+                status = _("Authentication required")
             case [
-                NM.DeviceState.CONFIG,
                 NM.DeviceState.PREPARE,
-                NM.DeviceState.NEED_AUTH,
+                NM.DeviceState.CONFIG,
                 NM.DeviceState.IP_CONFIG,
                 NM.DeviceState.IP_CHECK,
                 NM.DeviceState.SECONDARIES,
@@ -131,8 +175,12 @@ class VanillaDefaultNetwork(Adw.Bin):
                 status = _("Disconnecting")
             case NM.DeviceState.FAILED:
                 status = _("Connection Failed")
-            case _:
-                status = _("Unknown")
+            case NM.DeviceState.UNKNOWN:
+                status = _("Status Unknown")
+            case NM.DeviceState.UNMANAGED:
+                status = _("Unmanaged")
+            case NM.DeviceState.UNAVAILABLE:
+                status = _("Unavailable")
 
         return status, connected
 
@@ -144,6 +192,42 @@ class VanillaDefaultNetwork(Adw.Bin):
         else:
             self.has_eth_connection = False
 
+        # Wired devices with no cable plugged in are shown as unavailable
+        if conn.get_state() == NM.DeviceState.UNAVAILABLE:
+            status = _("Cable Unplugged")
+
         eth_conn = Adw.ActionRow(title=status)
         self.wired_group.add(eth_conn)
         self.__wired_children.append(eth_conn)
+
+    def __add_wifi_networks(self, conn: NM.DeviceWifi):
+        def scan_callback(source_object, res, *data):
+            while source_object.get_last_scan() == self.__last_wifi_scan:
+                time.sleep(0.25)
+
+            networks = {}
+            for ap in source_object.get_access_points():
+                ssid = ap.get_ssid()
+                if ssid is None:
+                    continue
+
+                ssid = ssid.get_data().decode("utf-8")
+                if ssid in networks.keys():
+                    networks[ssid].append(ap)
+                else:
+                    networks[ssid] = [ap]
+
+            for ssid, aps in networks.items():
+                max_strength = -1
+                best_ap = None
+                for ap in aps:
+                    ap_strength = ap.get_strength()
+                    if ap_strength > max_strength:
+                        max_strength = ap_strength
+                        best_ap = ap
+
+                wifi_network = WirelessRow(best_ap)
+                self.wireless_group.add(wifi_network)
+                self.__wireless_children.append(wifi_network)
+
+        conn.request_scan_async(callback=scan_callback)
