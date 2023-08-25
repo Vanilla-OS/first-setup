ebc24efa724ed671df419cad97ba13e95e1bafe6
Mateus Melchiades <matbme@duck.com>
2023-08-25 19:50:09 -0300

    Allow connecting to wireless devices

--- vanilla_first_setup/defaults/network.py
+++ vanilla_first_setup/defaults/network.py
@@ -16,14 +16,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import time
 from gettext import gettext as _
-from threading import Timer
+from threading import Lock, Timer
 
-from gi.repository import NM, Adw, Gtk
+from gi.repository import NM, NMA4, Adw, Gtk
 
 from vanilla_first_setup.utils.run_async import RunAsync
 
+
+logger = logging.getLogger("FirstSetup::Network")
 
 # Dictionary mapping security types to a tuple containing
 # their pretty name and whether it is a secure protocol.
@@ -42,9 +45,13 @@ AP_SECURITY_TYPES = {
 # PyGObject-libnm doesn't seem to expose these values, so we have redefine them
 NM_802_11_AP_FLAGS_PRIVACY = 0x00000001
 NM_802_11_AP_SEC_NONE = 0x00000000
-NM_802_11_AP_SEC_KEY_MGMT_SAE = 0x00000400
+
+NM_802_11_AP_SEC_KEY_MGMT_802_1X = 0x00000200
+NM_802_11_AP_SEC_KEY_MGMT_EAP_SUITE_B_192 = 0x00002000
 NM_802_11_AP_SEC_KEY_MGMT_OWE = 0x00000800
 NM_802_11_AP_SEC_KEY_MGMT_OWE_TM = 0x00001000
+NM_802_11_AP_SEC_KEY_MGMT_PSK = 0x00000100
+NM_802_11_AP_SEC_KEY_MGMT_SAE = 0x00000400
 
 
 @Gtk.Template(resource_path="/org/vanillaos/FirstSetup/gtk/wireless-row.ui")
@@ -55,10 +62,16 @@ class WirelessRow(Adw.ActionRow):
     secure_icon = Gtk.Template.Child()
     connected_label = Gtk.Template.Child()
 
-    def __init__(self, ap: NM.AccessPoint, **kwargs):
+    def __init__(self, window, client, device: NM.DeviceWifi, ap, **kwargs):
         super().__init__(**kwargs)
+        self.__window = window
+        self.client = client
         self.ap = ap
+        self.device = device
         self.refresh_ui()
+
+        self.set_activatable(True)
+        self.connect("activated", self.__show_connect_dialog)
 
     @property
     def ssid(self):
@@ -90,11 +103,22 @@ class WirelessRow(Adw.ActionRow):
             if not secure:
                 self.secure_icon.set_from_icon_name("warning-small-symbolic")
             else:
-                self.secure_icon.set_from_icon_name("network-wireless-encrypted-symbolic")
+                self.secure_icon.set_from_icon_name(
+                    "network-wireless-encrypted-symbolic"
+                )
 
         self.secure_icon.set_visible(secure is not None)
         if tooltip is not None:
             self.secure_icon.set_tooltip_text(tooltip)
+
+        self.connected_label.set_visible(self.is_connected())
+
+    def is_connected(self):
+        active_connection = self.device.get_active_connection()
+        if active_connection is not None:
+            if active_connection.get_id() == self.ssid:
+                return True
+        return False
 
     def __get_security(self) -> tuple[bool | None, str | None]:
         flags = self.ap.get_flags()
@@ -129,6 +153,83 @@ class WirelessRow(Adw.ActionRow):
         else:
             return AP_SECURITY_TYPES["wpa2"]
 
+    @property
+    def __key_mgmt(self):
+        # Key management used for the connection. One of "none" (WEP or no
+        # password protection), "ieee8021x" (Dynamic WEP), "owe" (Opportunistic
+        # Wireless Encryption), "wpa-psk" (WPA2 + WPA3 personal), "sae" (WPA3
+        # personal only), "wpa-eap" (WPA2 + WPA3 enterprise) or
+        # "wpa-eap-suite-b-192" (WPA3 enterprise only).
+        rsn_flags = self.ap.get_rsn_flags()
+        wpa_flags = self.ap.get_wpa_flags()
+
+        if wpa_flags == NM_802_11_AP_SEC_NONE and rsn_flags == NM_802_11_AP_SEC_NONE:
+            return "none"
+        elif rsn_flags & NM_802_11_AP_SEC_KEY_MGMT_802_1X:
+            return "ieee8021x"
+        elif rsn_flags & NM_802_11_AP_SEC_KEY_MGMT_EAP_SUITE_B_192:
+            return "wpa-eap-suite-b-192"
+        elif (
+            rsn_flags & NM_802_11_AP_SEC_KEY_MGMT_OWE
+            or rsn_flags & NM_802_11_AP_SEC_KEY_MGMT_OWE_TM
+        ):
+            return "owe"
+        elif rsn_flags & NM_802_11_AP_SEC_KEY_MGMT_PSK:
+            return "wpa-psk"
+        elif rsn_flags & NM_802_11_AP_SEC_KEY_MGMT_SAE:
+            return "sae"
+
+    def __show_connect_dialog(self, data):
+        dialog = NMA4.WifiDialog.new(
+            self.client, self.__construct_connection(), self.device, self.ap, False
+        )
+        dialog.set_modal(True)
+        dialog.set_transient_for(self.__window)
+
+        dialog.connect("response", self.__on_dialog_response)
+
+        dialog.show()
+
+    def __on_dialog_response(self, dialog, response_id):
+        def connect_cb(client, result, data):
+            try:
+                ac = client.add_and_activate_connection_finish(result)
+                logger.debug("ActiveConnection {}".format(ac.get_path()))
+            except Exception as e:
+                logger.error("Error:", e)
+
+        if response_id == -6:
+            dialog.close()
+        elif response_id == -5:
+            conn, _, _ = dialog.get_connection()
+            self.client.add_and_activate_connection_async(
+                conn, self.device, self.ap.get_path(), None, connect_cb, None
+            )
+            dialog.close()
+
+    def __construct_connection(self):
+        connection = NM.SimpleConnection.new()
+        s_con = NM.SettingConnection.new()
+        s_con.set_property(NM.SETTING_CONNECTION_ID, self.ssid)
+        s_con.set_property(NM.SETTING_CONNECTION_TYPE, "802-11-wireless")
+        s_wifi = NM.SettingWireless.new()
+        s_wifi.set_property(NM.SETTING_WIRELESS_SSID, self.ap.get_ssid())
+        s_wifi.set_property(NM.SETTING_WIRELESS_MODE, "infrastructure")
+        s_wsec = NM.SettingWirelessSecurity.new()
+        s_wsec.set_property(NM.SETTING_WIRELESS_SECURITY_KEY_MGMT, self.__key_mgmt)
+        s_ip4 = NM.SettingIP4Config.new()
+        s_ip4.set_property(NM.SETTING_IP_CONFIG_METHOD, "auto")
+        s_ip6 = NM.SettingIP6Config.new()
+        s_ip6.set_property(NM.SETTING_IP_CONFIG_METHOD, "auto")
+
+        connection.add_setting(s_con)
+        connection.add_setting(s_wifi)
+        connection.add_setting(s_wsec)
+        connection.add_setting(s_ip4)
+        connection.add_setting(s_ip6)
+
+        return connection
+
 
 @Gtk.Template(resource_path="/org/vanillaos/FirstSetup/gtk/default-network.ui")
 class VanillaDefaultNetwork(Adw.Bin):
@@ -153,6 +254,12 @@ class VanillaDefaultNetwork(Adw.Bin):
         self.__wireless_children = {}
 
         self.__last_wifi_scan = 0
+
+        # Prevent concurrency issues when re-scanning Wi-Fi devices.
+        # Since we reload the list every time there's a state change,
+        # there's a high change that it coincides with a periodic
+        # refresh operation.
+        self.__wifi_lock = Lock()
 
         # Since we have a dedicated page for checking connectivity,
         # we only need to make sure the user has some type of
@@ -195,6 +302,7 @@ class VanillaDefaultNetwork(Adw.Bin):
                     self.__add_ethernet_connection(device)
                     eth_devices += 1
                 elif device_type == NM.DeviceType.WIFI:
+                    device.connect("state-changed", self.__on_state_changed)
                     self.__refresh_wifi_list(device)
                     wifi_devices += 1
                 else:
@@ -210,6 +318,9 @@ class VanillaDefaultNetwork(Adw.Bin):
 
     def __remove_device(self, client, device):
         self.__devices.remove(device)
+
+    def __on_state_changed(self, device, new_state, old_state, reason):
+        self.__refresh()
 
     def __refresh(self):
         for child in self.__wired_children:
@@ -295,6 +406,8 @@ class VanillaDefaultNetwork(Adw.Bin):
             else:
                 networks[ssid] = [ap]
 
+        self.__wifi_lock.acquire()
+
         # Invalidate current list
         for ssid, (child, clean) in self.__wireless_children.items():
             self.__wireless_children[ssid] = (child, True)
@@ -317,7 +430,7 @@ class VanillaDefaultNetwork(Adw.Bin):
                 continue
 
             # Create new row if SSID is new
-            wifi_network = WirelessRow(best_ap)
+            wifi_network = WirelessRow(self.__window, self.__nm_client, conn, best_ap)
             self.wireless_group.add(wifi_network)
             self.__wireless_children[ssid] = (wifi_network, False)
 
@@ -331,10 +444,11 @@ class VanillaDefaultNetwork(Adw.Bin):
         for ssid in invalid_ssids:
             del self.__wireless_children[ssid]
 
+        self.__wifi_lock.release()
+
     def __scan_wifi(self, conn: NM.DeviceWifi):
         self.__last_wifi_scan = conn.get_last_scan()
         conn.request_scan_async()
-        print("Running scan")
 
         t = Timer(1.5, self.__refresh_wifi_list, [conn])
         t.start()
